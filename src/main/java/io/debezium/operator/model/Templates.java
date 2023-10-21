6db1daa4d37ef8c37ce8d7788e5afe974b567489
Jakub Cechacek <jcechace@gmail.com>
2023-10-21 19:23:00 +0200

    DBZ-7052 Initial support for container templating

--- src/main/java/io/debezium/operator/model/Templates.java
+++ src/main/java/io/debezium/operator/model/Templates.java
@@ -6,16 +6,22 @@
 package io.debezium.operator.model;
 
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import io.debezium.operator.model.templates.ContainerTemplate;
 import io.debezium.operator.model.templates.PodTemplate;
 
+@JsonPropertyOrder({ "container", "pod" })
 public class Templates {
 
+    @JsonPropertyDescription("Container template")
+    private ContainerTemplate container;
     @JsonPropertyDescription("Pod template.")
     private PodTemplate pod;
 
     public Templates() {
         this.pod = new PodTemplate();
+        this.container = new ContainerTemplate();
     }
 
     public PodTemplate getPod() {
@@ -24,5 +30,13 @@ public class Templates {
 
     public void setPod(PodTemplate pod) {
         this.pod = pod;
+    }
+
+    public ContainerTemplate getContainer() {
+        return container;
+    }
+
+    public void setContainer(ContainerTemplate container) {
+        this.container = container;
     }
 }
