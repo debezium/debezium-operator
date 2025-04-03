be06b507d84ed1c4e170190954a0fea4b56710be
Fiore Mario Vitale <mvitale@redhat.com>
2025-04-03 11:56:50 +0200

    DBZ-8857 Remove lombok dependency

--- systemtests/src/test/java/io/debezium/operator/systemtests/resources/server/DebeziumServerList.java
+++ systemtests/src/test/java/io/debezium/operator/systemtests/resources/server/DebeziumServerList.java
@@ -8,9 +8,6 @@ package io.debezium.operator.systemtests.resources.server;
 import io.debezium.operator.api.model.DebeziumServer;
 import io.fabric8.kubernetes.api.model.DefaultKubernetesResourceList;
 
-import lombok.ToString;
-
-@ToString(callSuper = true)
 public class DebeziumServerList extends DefaultKubernetesResourceList<DebeziumServer> {
     private static final long serialVersionUID = 1L;
 }
